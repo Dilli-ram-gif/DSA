3feec6dc0b015784249e50bbb94dd5922a23c687
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-09-08 09:15:27 +0545

    test commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int number = 2;
+  float price = 50.50;
+  char first_letter = 'a';
+  bool isEligible = true;
+
+  double rate = 100.25;
+
+  cout << number << endl;
+
+  return 0;
+}
+//This is the test file
---
+++ pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// This is the normal program to  print the pattern using C++
+int main()
+{
+
+  int num = 5;
+  for (int i = 0; i < count; i++)
+  {
+  }
+
+  return 0;
+}
